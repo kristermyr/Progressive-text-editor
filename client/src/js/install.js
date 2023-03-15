1d0f10559516d3e9e7b84f138824cd76c806f539
kristermyr <110065764+kristermyr@users.noreply.github.com>
2023-03-15 12:58:13 -0600

    fixing install button not working

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,12 +1,29 @@
 const butInstall = document.getElementById('buttonInstall');
-
+// from 28-Stu_Mini-Project
 // Logic for installing the PWA
 
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// xTODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// xTODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+   return;
+  }
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
+});
+
+
+// xTODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
