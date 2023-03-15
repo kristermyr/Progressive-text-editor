1d8abcfe0abab54224816d422aa2728ba7ebff92
kristermyr <110065764+kristermyr@users.noreply.github.com>
2023-03-15 12:46:31 -0600

    working on making install button work

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,4 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-
 const { CacheFirst,StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
@@ -30,16 +29,19 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache),
 // TODO: Implement asset caching
   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
   
-  new StaleWhileRevalidate({
-    // Name of the cache storage.
-    cacheName: 'asset-cache',
-    plugins: [
-      // This plugin will cache responses with these headers to a maximum-age of 30 days
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  });
+  registerRoute(
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+    new StaleWhileRevalidate({
+      // Name of the cache storage.
+      cacheName: 'asset-cache',
+      plugins: [
+        // This plugin will cache responses with these headers to a maximum-age of 30 days
+        new CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+      ],
+    })
+  );
 
 
-registerRoute();
+//registerRoute();
